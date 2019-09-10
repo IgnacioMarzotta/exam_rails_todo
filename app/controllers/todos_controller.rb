4e145b91ba7d725112744bfb2e6d00635ee39da3
Ignacio Marzotta <marzottanacho@gmail.com>
2019-09-10 10:57:35 -0300

    Created 'Show' and 'Create' function with their respective views and functions

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,9 +1,28 @@
 class TodosController < ApplicationController
+    before_action :find_todo, only: [:show]
+
     def index
         @todos = Todo.all
-    end 
+    end
 
     def new
         @todo = Todo.new
+    end
+
+    def create
+        @todo = Todo.new(todo_params)
+        @todo.save
+        redirect_to todos_path
+    end
+
+    def show; end
+
+    def find_todo
+        @todo = Todo.find(params[:id])
+    end
+
+    private
+    def todo_params
+        params.require(:todo).permit(:description, :completed)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get 'todos', to: 'todos#index'
   get 'todos/new'
+  post 'todos', to: 'todos#create'
+  get 'todos/:id', as: 'todo', to: 'todos#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
